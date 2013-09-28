009864e352f6657d208489a3188d91d489d968d3
Seth Urban <surban@prototest.com>
2013-09-27 20:39:24 -0600

    Added Content screens

---
+++ screenobjects-happy/src/main/java/com/prototest/appium/Makr_Content_Creation_Screen.java
@@ -0,0 +1,17 @@
+package com.prototest.appium;
+
+import com.prototest.appiumcore.*;
+import org.openqa.selenium.By;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Seth
+ * Date: 9/27/13
+ * Time: 8:35 PM
+ * This screen is used to create some content
+ */
+public class Makr_Content_Creation_Screen extends appiumScreenBase {
+    //This is actually on the screen before this screen
+    appElement SelectTemplate = new appElement("SelectTemplate", By.xpath("//window[1]/button[9]"));
+
+}
