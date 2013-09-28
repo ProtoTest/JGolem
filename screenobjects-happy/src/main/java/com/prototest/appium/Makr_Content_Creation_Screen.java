8d72e146823570928f3cc413988d71aff6772af4
Seth Urban <surban@prototest.com>
2013-09-27 21:47:33 -0600

    Content Creation Screen

--- screenobjects-happy/src/main/java/com/prototest/appium/Makr_Content_Creation_Screen.java
+++ screenobjects-happy/src/main/java/com/prototest/appium/Makr_Content_Creation_Screen.java
@@ -11,7 +11,24 @@ import org.openqa.selenium.By;
  * This screen is used to create some content
  */
 public class Makr_Content_Creation_Screen extends appiumScreenBase {
-    //This is actually on the screen before this screen
+    //This is actually on the screen before this screen -- this is the button to get to the content screen
     appElement SelectTemplate = new appElement("SelectTemplate", By.xpath("//window[1]/button[9]"));
+
+    appElement UserMenu = new appElement("UserMenu", By.xpath("//window[1]/button[1]"));
+    appElement CloseButton = new appElement("CloseButton", By.name("CLOSE"));
+    appElement SaveButton = new appElement("SaveButton", By.name("SAVE"));
+    appElement MakeButton = new appElement("MakeButton", By.name("make!"));
+
+    appElement UndoButton = new appElement("UndoButton", By.xpath("//window[1]/button[5]"));
+    appElement RedoButton = new appElement("RedoButton", By.xpath("//window[1]/button[6]"));
+
+    appElement CreationTitle = new appElement("CreationTitle", By.xpath("//window[1]/text[2]"));
+    appElement FrontButton = new appElement("FrontButton", By.name("FRONT"));
+    appElement BackButton = new appElement("BackButton", By.name("BACK"));
+
+    appElement addText = new appElement("AddText", By.xpath("//window[1]/button[9]"));
+    appElement addArt = new appElement("addArt", By.xpath("//window[1]/button[9]"));
+
+
 
 }
