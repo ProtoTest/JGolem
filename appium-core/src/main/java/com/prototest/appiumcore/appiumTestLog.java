0542a9a1f427244e14cd9909a24517b42219915a
Seth <surban@prototest.com>
2013-10-07 18:01:24 -0600

    Trying to add TestLog Class

---
+++ appium-core/src/main/java/com/prototest/appiumcore/appiumTestLog.java
@@ -0,0 +1,11 @@
+package com.prototest.appiumcore;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sethurban
+ * Date: 10/7/13
+ * Time: 4:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class appiumTestLog {
+}
